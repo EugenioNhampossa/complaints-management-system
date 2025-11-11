8d9b73d00e33ff861ff06f476714c6d74832d85d
AlguemDaRua <azamusman@proton.me>
2025-11-12 00:53:33 +0200

    na estrutura de login

--- components/form/user/register.form.tsx
+++ components/form/user/register.form.tsx
@@ -8,16 +8,51 @@ import {
   Checkbox,
 } from "@mantine/core";
 import { IconAt, IconLock, IconUser, IconPhone } from "@tabler/icons-react";
+import { useForm } from "@mantine/form";
+import { useRouter } from "next/navigation";
 
 export function RegisterForm() {
+  const router = useRouter();
+
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      phone: "",
+      password: "",
+      confirmPassword: "",
+      terms: false,
+    },
+    validate: {
+      name: (value) => 
+        value.length < 3 ? "O nome deve ter pelo menos 3 caracteres" : null,
+      email: (value) => 
+        !/^\S+@\S+$/.test(value) ? "Email inválido" : null,
+      phone: (value) =>
+        value.length < 9 ? "Número de telefone inválido" : null,
+      password: (value) =>
+        value.length < 6 ? "A senha deve ter pelo menos 6 caracteres" : null,
+      confirmPassword: (value, values) =>
+        value !== values.password ? "As senhas não coincidem" : null,
+      terms: (value) =>
+        !value ? "Deve aceitar os termos e condições" : null,
+    },
+  });
+
+  const handleSubmit = (values: typeof form.values) => {
+    console.log("Registro:", values);
+
+  };
+
   return (
-    <form>
+    <form onSubmit={form.onSubmit(handleSubmit)}>
       <Stack>
         <TextInput
           label="Nome completo"
           placeholder="Digite seu nome completo"
           leftSection={<IconUser size={16} />}
           required
+          {...form.getInputProps("name")}
         />
 
         <TextInput
@@ -25,13 +60,15 @@ export function RegisterForm() {
           placeholder="seu.email@exemplo.com"
           leftSection={<IconAt size={16} />}
           required
+          {...form.getInputProps("email")}
         />
 
         <TextInput
           label="Telefone"
-          placeholder="8x 123 4567"
+          placeholder="84 123 4567"
           leftSection={<IconPhone size={16} />}
           required
+          {...form.getInputProps("phone")}
         />
 
         <PasswordInput
@@ -39,6 +76,7 @@ export function RegisterForm() {
           placeholder="Digite sua senha"
           leftSection={<IconLock size={16} />}
           required
+          {...form.getInputProps("password")}
         />
 
         <PasswordInput
@@ -46,10 +84,12 @@ export function RegisterForm() {
           placeholder="Digite a senha novamente"
           leftSection={<IconLock size={16} />}
           required
+          {...form.getInputProps("confirmPassword")}
         />
 
         <Checkbox
           label="Aceito os termos e condições de uso"
+          {...form.getInputProps("terms", { type: "checkbox" })}
         />
 
         <Button type="submit" fullWidth size="md">
